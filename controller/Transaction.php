de60c8ab7f7f95c98ce609c9c686077f18bc6d0c
Leo Nadeak <lnadeak97@gmail.com>
2020-01-19 21:59:04 +0700

    add transaction controller

--- controller/Transaction.php
+++ controller/Transaction.php
@@ -1,3 +1,15 @@
 <?php
 
+namespace controller;
+
+use repository\Transaction as TransactionRepo;
+
+class Transaction {
+    private $transactionRepo;
+
+    function __construct() {
+        $this->transactionRepo = new TransactionRepo;
+    }
+}
+
 ?>
